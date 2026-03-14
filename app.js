a10f7da80abbfaa7dcd6a8eca24cb4adae512210
truongduy-code <duynguyen.280905@gmail.com>
2026-03-14 14:56:21 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const cors = require("cors");
 
 const contactRouter = require("./app/routes/contact.route");
+const ApiError = require("./app/api-error");
+
 const app = express();
 
 app.use(cors());
@@ -11,6 +13,19 @@ app.get('/', (req, res) => {
     res.json({ message: "Welcome to contact book application!" });
 });
 
+// routes
 app.use("/api/contacts", contactRouter);
+
+// handle 404
+app.use((req, res, next) => {
+    return next(new ApiError(404, "Resource not found"));
+});
+
+// error handler
+app.use((err, req, res, next) => {
+    return res.status(err.statusCode || 500).json({
+    message: err.message || "Internal Server Error",
+    });
+});
 
 module.exports = app;
